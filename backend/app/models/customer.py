4f3487a518f22028bf52a1e2c4f878c7dae5354b
Adam <adam.shaw003@gmail.com>
2026-04-21 00:01:26 -0600

    File update

--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -1,5 +1,4 @@
-# SQL Alchemy handles turning into correct SQL insert
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 
 from app.database import Base
 
@@ -7,8 +6,8 @@ from app.database import Base
 class CustomerAccount(Base):
     __tablename__ = "CUSTOMER_ACCOUNT"
 
-    email = Column(String(255), primary_key=True, nullable=False)
-    phoneNumber = Column(String(50))
-    name = Column(String(100), nullable=False)
-    username = Column(String(50), nullable=False, unique=True)
-    passwordHash =a Column(String(255), nullable=False)
+    email        = Column(String(255), primary_key=True, nullable=False)
+    phoneNumber  = Column(String(50))
+    name         = Column(String(100), nullable=False)
+    username     = Column(String(50),  nullable=False)
+    passwordHash = Column(String(255), nullable=False) # passwordHash instead
